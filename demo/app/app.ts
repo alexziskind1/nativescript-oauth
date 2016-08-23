7e87c43c9225726dcf417e71459f8d1751945a67
Alexander Ziskind <alex@nuvious.com>
2016-08-23 10:12:45 -0400

    Demo app updated to use latest

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -3,17 +3,17 @@ import * as tnsOAuthModule from 'nativescript-oauth';
 
 
 var o365InitOptions : tnsOAuthModule.ITnsOAuthOptionsOffice365 = {
-    clientId: 'd07edd43-95dd-4cd3-bcab-75cbbc1c7431', //client id for application (GUID)
+    clientId: '99e1c318-a133-482f-a53b-5122ceab4c99', //client id for application (GUID)
     scope: ['Files.ReadWrite', 'User.ReadWrite', 'offline_access']
 };
 
 var facebookInitOptions : tnsOAuthModule.ITnsOAuthOptionsFacebook = {
-    clientId: '1819818654921817',
-    clientSecret: 'b7e58f212b51e4d639bed857171c992a',
+    clientId: '996054078783499',
+    clientSecret: '99390a088882d611c8d585dfab17cd99',
     scope: ['email']
 };
 
-tnsOAuthModule.initOffice365(o365InitOptions);
-//tnsOAuthModule.initFacebook(facebookInitOptions);
+//tnsOAuthModule.initOffice365(o365InitOptions);
+tnsOAuthModule.initFacebook(facebookInitOptions);
 
 application.start({ moduleName: 'main-page' });
