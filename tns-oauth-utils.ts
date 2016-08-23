d3b18b608aadbf98dfc548ed5788123dfa0269a0
Alexander Ziskind <alex@nuvious.com>
2016-08-23 10:12:30 -0400

    Utils fixes

--- tns-oauth-utils.ts
+++ tns-oauth-utils.ts
@@ -15,3 +15,5 @@ export function nsArrayToJSArray(a) {
     }
     return arr;
 }
+
+export function newUUID(a?,b?){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b}
