32cf1e9c9da2124b2ac5a0e16b57672a446cb4aa
Alexander Ziskind <alex@nuvious.com>
2016-08-23 08:01:03 -0400

    Updated demo project to use latest

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,12 +1,13 @@
 ﻿import * as application from 'application';
 import * as tnsOAuthModule from 'nativescript-oauth';
 
-var o365InitOptions : TnsOAuth.ITnsOAuthOptionsOffice365 = {
+
+var o365InitOptions : tnsOAuthModule.ITnsOAuthOptionsOffice365 = {
     clientId: 'd07edd43-95dd-4cd3-bcab-75cbbc1c7431', //client id for application (GUID)
     scope: ['Files.ReadWrite', 'User.ReadWrite', 'offline_access']
 };
 
-var facebookInitOptions : TnsOAuth.ITnsOAuthOptionsFacebook = {
+var facebookInitOptions : tnsOAuthModule.ITnsOAuthOptionsFacebook = {
     clientId: '1819818654921817',
     clientSecret: 'b7e58f212b51e4d639bed857171c992a',
     scope: ['email']
