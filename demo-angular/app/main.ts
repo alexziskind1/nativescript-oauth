3bbce478d8bfe508d24276e082467814a6efcb41
Tim Morris <e12421@EXPCH2779.lan>
2017-03-10 17:32:33 -0500

    Misc. cleanup

--- demo-angular/app/main.ts
+++ demo-angular/app/main.ts
@@ -28,11 +28,6 @@ let uaaInitOptions: tnsOAuthModule.ITnsOAuthOptionsUaa = {
     
 }
 
-
-// basicAuthHeader: 'Basic dGltLWNsaWVudDo0Kmh3cVRDIzl2TTU5Sw=='
-// tim-client:4*hwqTC$RFRr5#9vM59K
-// Basic dGltLWNsaWVudDo0Kmh3cVRDJFJGUnI1Izl2TTU5Sw==
-
 // tnsOAuthModule.initOffice365(o365InitOptions);
 // tnsOAuthModule.initFacebook(facebookInitOptions);
 
@@ -41,11 +36,3 @@ tnsOAuthModule.initUaa(uaaInitOptions);
 
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
-
-
-
-
-
-
-
-// curl 'https://5abc6901-aa46-41af-8870-7aaf4290d610.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token' -i -u 'login:loginsecret' -X POST -H 'Accept: application/json' -H 'Content-Type: application/x-www-form-urlencoded' -d 'client_id=tim-client&client_secret=4*hwqTC$RFRr5#9vM59K&grant_type=authorization_code&response_type=token&code=WeeQgWDi2x&token_format=opaque&redirect_uri=udentifyclient://onauthcallback'
