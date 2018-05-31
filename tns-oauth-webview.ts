3edc711e16b3c94081e1ec0e348a9ea8d60c7f09
interrobrian <brianspencer@outlook.com>
2018-05-31 09:30:10 -0600

    Matched indentation on new code to the rest of the project

--- tns-oauth-webview.ts
+++ tns-oauth-webview.ts
@@ -3,14 +3,14 @@
 import { WebView } from 'ui/web-view';
 
 export class TnsOauthWebView extends WebView {
-  constructor(
-    private _cancelEventHandler: (error?) => void
-  ) {
-    super();
-  }
+    constructor(
+        private _cancelEventHandler: (error?) => void
+    ) {
+        super();
+    }
 
-  public onUnloaded() {
-    super.onUnloaded();
-    this._cancelEventHandler("User cancelled.");
-  }
+    public onUnloaded() {
+        super.onUnloaded();
+        this._cancelEventHandler("User cancelled.");
+    }
 }
